38632e153c786e4c7d34945c9015ec48c0bf7219
guangtao.wang <guangtao.wang@jd.com>
2021-09-09 10:23:52 -0700

    Initial commit

---
+++ gen_randsearch_jobs.py
@@ -0,0 +1,10 @@
+from hyperparatuner.rand_search import generate_random_search_bash
+import argparse
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    # Required parameters
+    parser.add_argument("--rand_seed", type=int, default=888)
+    parser.add_argument("--task_num", type=int, default=1)
+    args = parser.parse_args()
+    generate_random_search_bash(task_num=args.task_num, seed=args.rand_seed)
