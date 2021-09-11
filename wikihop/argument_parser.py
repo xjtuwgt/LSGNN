de0818be330a334bff2730af700b188b2d2573fb
guangtao.wang <guangtao.wang@jd.com>
2021-09-11 14:13:11 -0700

    Initial commit

--- wikihop/argument_parser.py
+++ wikihop/argument_parser.py
@@ -133,7 +133,7 @@ def default_parser():
     parser.add_argument('--alpha', type=float, default=0.1)
     parser.add_argument('--hidden_dim', type=int, default=512)
     parser.add_argument('--head_num', type=int, default=8)
-    parser.add_argument('--layers', type=int, default=3)
+    parser.add_argument('--layers', type=int, default=6)
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     parser.add_argument('--eval_batch_size', type=int, default=32)
     parser.add_argument('--per_gpu_train_batch_size', type=int, default=16)
