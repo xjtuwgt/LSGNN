2a8001be54333a889127c299054d8919f3cb3e6c
guangtao.wang <guangtao.wang@jd.com>
2021-09-13 13:32:26 -0700

    Initial commit

--- wikihop/argument_parser.py
+++ wikihop/argument_parser.py
@@ -130,7 +130,7 @@ def default_parser():
     parser.add_argument('--stanford', type=boolean_string, default='true')
 
     parser.add_argument('--hop_num', type=int, default=4)
-    parser.add_argument('--alpha', type=float, default=0.15)
+    parser.add_argument('--alpha', type=float, default=0.1)
     parser.add_argument('--hidden_dim', type=int, default=512)
     parser.add_argument('--head_num', type=int, default=8)
     parser.add_argument('--layers', type=int, default=3)
