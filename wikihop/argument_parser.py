e91f097eef7ec25ffb87769f7511180e62ab9fb8
guangtao.wang <guangtao.wang@jd.com>
2021-09-17 13:43:09 -0700

    Initial commit

--- wikihop/argument_parser.py
+++ wikihop/argument_parser.py
@@ -127,12 +127,12 @@ def default_parser():
     parser.add_argument('--lstm_layers', type=int, default=2)
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    parser.add_argument('--tcn_layers', type=int, default=2)
+    parser.add_argument('--tcn_layers', type=int, default=3)
     parser.add_argument('--tcn_conv_drop', type=float, default=0.2)
     parser.add_argument('--tcn_fc_drop', type=float, default=0.2)
-    parser.add_argument('--tcn_hid_dim', type=int, default=64)
-    parser.add_argument('--tcn_out_dim', type=int, default=512)
-    parser.add_argument('--tcn_kernel_size', type=int, default=7)
+    parser.add_argument('--tcn_hid_dim', type=int, default=32)
+    parser.add_argument('--tcn_out_dim', type=int, default=256)
+    parser.add_argument('--tcn_kernel_size', type=int, default=3)
     # ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     parser.add_argument('--window_size', type=int, default=64)
     parser.add_argument('--sent_drop_prob', type=float, default=0.1)
