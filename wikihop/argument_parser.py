efad24164f4ed94629900d244fb2427ecb822573
guangtao.wang <guangtao.wang@jd.com>
2021-09-18 17:33:38 -0700

    Initial commit

--- wikihop/argument_parser.py
+++ wikihop/argument_parser.py
@@ -152,7 +152,7 @@ def default_parser():
     parser.add_argument('--ppr_diff', type=boolean_string, default='true')
     parser.add_argument('--stanford', type=boolean_string, default='true')
 
-    parser.add_argument('--hop_num', type=int, default=4)
+    parser.add_argument('--hop_num', type=int, default=3)
     parser.add_argument('--alpha', type=float, default=0.15)
     parser.add_argument('--hidden_dim', type=int, default=768)
     parser.add_argument('--head_num', type=int, default=12)
